3c4a4355a5160767b48dc1ca8025f0d35717be55
Ondrej Skrovan <ondrej.skrovan@erstegroupit.com>
2013-06-12 17:47:55 +0200

    ups

--- AndrIDE/src/main/java/com/oskovran/andride/AndrideActivity.java
+++ AndrIDE/src/main/java/com/oskovran/andride/AndrideActivity.java
@@ -2,14 +2,29 @@ package com.oskovran.andride;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.content.res.AssetManager;
+
+import java.io.InputStream;
 
 public class AndrideActivity extends Activity {
+
+    private static final String LOG_NAME = AndrideActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Read the contents of our asset
+        try {
+            AssetManager assets = getApplicationContext().getAssets();
+            InputStream is = assets.open("classes.dex");
+            Log.i(LOG_NAME, is.toString());
+        } catch(Exception e) {
+            Log.e(LOG_NAME, e.getMessage());
+        }
     }
 
 
